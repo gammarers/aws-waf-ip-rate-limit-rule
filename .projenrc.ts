4112a06a18a10c3b9a6f7a47de027846d189dae9
Yuichiro Higa <higa@avelio.co.jp>
2023-09-08 12:53:01 +0900

    fix!: jsii version set ~5.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
   typescriptVersion: '4.9.x',
+  jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-waf-ip-rate-limit-rule-group',
   projenrcTs: true,
